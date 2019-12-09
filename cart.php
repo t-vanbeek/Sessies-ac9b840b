b9d43127f9544c3e6dcab0201e4612f9f0bb46e9
Thorvald van Beek <tvanbeek@roc-dev.com>
2019-12-09 14:07:56 +0100

    Uitwerking van de exercise

--- cart.php
+++ cart.php
@@ -1,4 +1,21 @@
+<?php
+session_start();
+?>
 
+<!DOCTYPE html>
+<html lang="nl-NL">
+    <head>
+        <meta charset="UTF-8">
+        <title>cart.php</title>
+
+        <link href="bit_logo.png" rel="icon" type="image/png" />
+
+    </head>
+
+    <body>
+        <h1>Skateboard <small>(#1)</small></h1>
+        <h1>Basketbal <small>(#2)</small></h1>
+        <h1>Skeelers <small>(#3)</small></h1>
 
         <form method="POST">
             <input type="number" min="1" name="nummer" max="3" id="nummer" placeholder="Voer een nummer in" required size="1" />
@@ -6,11 +23,10 @@
         </form>
 
         <?php
-        session_start();
         if (isset($_POST["nummer"])) {
-            setcookie("nummer", $_POST["nummer"]);
+            $_SESSION["nummer"] = $_POST["nummer"];
             
-            echo $_REQUEST["nummer"];
+            echo $_SESSION["nummer"];
         }
         ?>
         
